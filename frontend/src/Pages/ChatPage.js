2a504378e20d6447ee97dfb59a51e39305ca33d0
Shvmvrshny <shvmvrshny@gmail.com>
2022-03-01 22:11:17 +0530

    Update ChatPage.js

--- frontend/src/Pages/ChatPage.js
+++ frontend/src/Pages/ChatPage.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
-import { useEffect, useState } from 'react'
 const ChatPage = () => {
   const [chats, setchats] = useState([])
   const fetchChats = async () => {
